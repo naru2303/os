6a94fc90c6a0bf5bbbdd6fd14b0093ad869b411b
robertmnovak <rnovakpublic@gmail.com>
2020-10-04 02:43:20 -0700

    Added startup file, linker script, and Makefile to build a LED blink program

--- Makefile
+++ Makefile
@@ -1,8 +1,18 @@
+CC=arm-none-eabi-gcc
+MACH=cortex-m4
+CFLAGS= -c -mcpu=$(MACH) -mthumb -std=gnu11 -Wall -o0
+LDFLAGS= -nostdlib -T st_ls.ld -Wl,-Map=main.map
 
+all:main.o st_startup.o main.elf
 
+main.o:main.c
+	$(CC) $(CFLAGS) -o $@ $^
 
+st_startup.o:st_startup.c
+	$(CC) $(CFLAGS) -o $@ $^
 
-HERE THE MAKEFILE WILL BE WRITTEN FOR ALL THE REP 
+main.elf: main.o st_startup.o
+	$(CC) $(LDFLAGS) -o $@ $^
 
-
-INCLUDING -O -CC -CFLAGs  - S -.c -.s and so on ... 
+clean:
+	rm -rf *.o *.elf
---
+++ main.c
@@ -0,0 +1,33 @@
+#include <stdint.h>
+#include "lib/stm32l4xx.h"
+
+void initPeriph(void);
+void delay(uint32_t time);
+
+/**
+*   @brief basic delay function
+*   @param time length of delay
+*   @return none
+*/
+void delay(uint32_t time)
+{
+    while(time > 0)
+    {
+        time--;
+    }
+}
+
+int main(void)
+{
+    RCC->AHB2ENR = (1<<1); /*< Enable GPIOB Clock */
+    GPIOB->MODER = (0x01 << 14); /*< Set PB7 as output */
+    GPIOB->ODR &= ~(1<<7); /*< Set PB7 to 0 */
+    while(1)
+    {
+        delay(500000);
+        GPIOB->ODR |= (1<<7);
+        delay(500000);
+        GPIOB->ODR &= ~(1<<7);
+    }
+    
+}
